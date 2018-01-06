7fe364a127fd89d0d91cc35d0ab67c1d20e92516
Ash Furrow <ash@ashfurrow.com>
2018-01-06 12:25:29 -0500

    Another typo.

--- rules/new-issue.ts
+++ rules/new-issue.ts
@@ -1,4 +1,4 @@
-import { warning } from "danger"
+import { warn } from "danger"
 // Add a check to see if a changelog exists, then warn if it isn't in modified etc
 
-warning("this is a test warning from Peril.")
+warn("this is a test warning from Peril.")
