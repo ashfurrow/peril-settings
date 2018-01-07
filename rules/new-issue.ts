4b2265ec67fd570cb7e9255ca216171fb3d5745a
Ash Furrow <ash@ashfurrow.com>
2018-01-07 16:25:57 -0500

    Added missing wrap from #4.

--- rules/new-issue.ts
+++ rules/new-issue.ts
@@ -9,17 +9,18 @@ const _test = (reason: string, promise: Promise<any>) => promise
 const _run = (reason: string, promise: Promise<any>) => schedule(promise)
 const wrap: any = isJest ? _test : _run
 
-// If the repo hasn't been updated for more than six months, then post a comment.
-export const markRepoAsStale = async () => {
-  const issue = danger.github.issue
-  const repoName = danger.github.thisPR.repo
-  const api = danger.github.api
-  const sixMonthsAgo = Date.now() - 3600 * 24 * 30 * 6
+export const markRepoAsStale = wrap(
+  "If the repo hasn't been updated for more than six months, then post a comment",
+  async () => {
+    const issue = danger.github.issue
+    const repoName = danger.github.thisPR.repo
+    const api = danger.github.api
+    const sixMonthsAgo = Date.now() - 3600 * 24 * 30 * 6
 
-  const repo = await api.repos.get({ repo: repoName, owner: "ashfurrow" })
-  // `pushed_at` is the last time that any commit was made to any branch.
-  if (Date.parse(repo.pushed_at) < sixMonthsAgo) {
-    markdown(`
+    const repo = await api.repos.get({ repo: repoName, owner: "ashfurrow" })
+    // `pushed_at` is the last time that any commit was made to any branch.
+    if (Date.parse(repo.pushed_at) < sixMonthsAgo) {
+      markdown(`
       Hey! It looks like this repo hasn't been updated for a while. That
       probably means the repo's not a high-priority for @ashfurrow. He'll answer 
       this issue if he can, but just a head's up.
@@ -29,5 +30,6 @@ export const markRepoAsStale = async () => {
       And of course, you're welcome to discuss with other developers in this
       repository's issues and pull requests. Have a great day!
     `)
+    }
   }
-}
+)
