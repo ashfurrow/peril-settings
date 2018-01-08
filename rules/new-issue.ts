04b2338b7e07764705759ce40c3b80459a9392f5
Ash Furrow <ash@ashfurrow.com>
2018-01-08 13:40:25 -0500

    Adds some logging for #7.

--- rules/new-issue.ts
+++ rules/new-issue.ts
@@ -1,4 +1,4 @@
-import { schedule, danger, markdown } from "danger"
+import { schedule, danger, markdown, warn } from "danger"
 import { Issues } from "github-webhook-event-types"
 
 // The inspiration for this is https://github.com/artsy/artsy-danger/blob/f019ee1a3abffabad65014afabe07cb9a12274e7/org/all-prs.ts
@@ -9,7 +9,7 @@ const _test = (reason: string, promise: Promise<any>) => promise
 const _run = (reason: string, promise: Promise<any>) => schedule(promise)
 const wrap: any = isJest ? _test : _run
 
-const message =`\
+const message = `\
 Hey! It looks like this repo hasn't been updated for a while. \
 That probably means the repo's not a high-priority for @ashfurrow. He'll answer \
 this issue if he can, but just a head's up.
@@ -28,6 +28,10 @@ export const markRepoAsStale = wrap(
     const sixMonthsAgo = Date.now() - 3600 * 24 * 30 * 6
 
     const result = await api.repos.get({ repo: repoName, owner: "ashfurrow" })
+    markdown(JSON.stringify(result))
+    warn(`result.data.pushed_at: ${result.data.pushed_at}`)
+    warn(`Date.parse(result.data.pushed_at): ${Date.parse(result.data.pushed_at)}`)
+    warn(`sixMonthsAgo: ${sixMonthsAgo}`)
     // `pushed_at` is the last time that any commit was made to any branch.
     if (Date.parse(result.data.pushed_at) < sixMonthsAgo) {
       markdown(message)
