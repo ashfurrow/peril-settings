c4fe445d37406f8ab6ea5bce1c084b3d7280c2fc
Ash Furrow <ash@ashfurrow.com>
2018-01-08 13:56:22 -0500

    Moves debugging info from 04b2338b7e07764705759ce40c3b80459a9392f5 into if statement.

--- rules/new-issue.ts
+++ rules/new-issue.ts
@@ -1,5 +1,6 @@
-import { schedule, danger, markdown, warn } from "danger"
+import { schedule, danger, markdown } from "danger"
 import { Issues } from "github-webhook-event-types"
+import { debug } from "util"
 
 // The inspiration for this is https://github.com/artsy/artsy-danger/blob/f019ee1a3abffabad65014afabe07cb9a12274e7/org/all-prs.ts
 const isJest = typeof jest !== "undefined"
@@ -28,13 +29,20 @@ export const markRepoAsStale = wrap(
     const sixMonthsAgo = Date.now() - 3600 * 24 * 30 * 6
 
     const result = await api.repos.get({ repo: repoName, owner: "ashfurrow" })
-    markdown(JSON.stringify(result))
-    warn(`result.data.pushed_at: ${result.data.pushed_at}`)
-    warn(`Date.parse(result.data.pushed_at): ${Date.parse(result.data.pushed_at)}`)
-    warn(`sixMonthsAgo: ${sixMonthsAgo}`)
     // `pushed_at` is the last time that any commit was made to any branch.
     if (Date.parse(result.data.pushed_at) < sixMonthsAgo) {
       markdown(message)
+      const debugMessage = `<details>
+        <summary>This is debug info for @ashfurrow. See https://github.com/ashfurrow/peril-settings/issues/6.</summary>
+
+        result.data.pushed_at: ${result.data.pushed_at}
+        Date.parse(result.data.pushed_at): ${Date.parse(result.data.pushed_at)}
+        sixMonthsAgo: ${sixMonthsAgo}
+        entire object: ${JSON.stringify(result)}
+
+        </details>
+        `
+      markdown(debugMessage)
     }
   }
 )
